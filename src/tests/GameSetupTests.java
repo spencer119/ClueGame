41a979fee958f3249f5fdb1f38d508d5b3a813f9
Spencer Hamilton <shamilton@mines.edu>
2023-04-02 12:48:32 -0600

    failing tests for player class created

--- src/tests/GameSetupTests.java
+++ src/tests/GameSetupTests.java
@@ -1,13 +1,15 @@
 package tests;
 
 import clueGame.Board;
-import clueGame.BoardCell;
+import clueGame.ComputerPlayer;
+import clueGame.HumanPlayer;
+import clueGame.Player;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Set;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 /**
@@ -26,5 +28,37 @@ public class GameSetupTests {
         board.initialize();
     }
 
+    @Test
+    public void testInitialize() {
+    }
 
+    @Test
+    public void testPlayers() {
+        ArrayList<Player> players = board.getPlayers();
+        // Check if the list contains 6 players in total
+        assertEquals(6, players.size());
+        // Check if the list contains 1 HumanPlayer and 5 ComputerPlayers.
+        int humanPlayerCount = 0;
+        int computerPlayerCount = 0;
+
+        for (Player player : players) {
+            if (player instanceof HumanPlayer) {
+                humanPlayerCount++;
+            } else if (player instanceof ComputerPlayer) {
+                computerPlayerCount++;
+            }
+        }
+
+        // Assert the counts are 1 and 5
+        assertEquals(1, humanPlayerCount);
+        assertEquals(5, computerPlayerCount);
+    }
+
+    @Test
+    public void testSolutions() {
+    }
+
+    @Test
+    public void testCards() {
+    }
 }
