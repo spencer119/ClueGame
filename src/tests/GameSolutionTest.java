cc2009788a0094db22e42b29a24dde966260d6bc
Spencer Hamilton <shamilton@mines.edu>
2023-04-05 15:26:22 -0600

    made failing suggestion tests

--- src/tests/GameSolutionTest.java
+++ src/tests/GameSolutionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,5 +37,41 @@ public class GameSolutionTest {
         assertFalse(board.checkAccusation(wrongPerson));
         Solution wrongRoom = new Solution(new Card("MZ 026", CardType.ROOM), boardSolution.getPerson(), boardSolution.getWeapon());
         assertFalse(board.checkAccusation(wrongRoom));
+    }
+
+    @Test
+    public void testSuggestions() {
+        Player testPlayer = new HumanPlayer("test", "red", 7, 7);
+        Player disprovePlayer = new HumanPlayer("disprove", "blue", 10, 10);
+
+        Card testPerson = new Card("testPerson", CardType.PERSON);
+        Card testRoom = new Card("testRoom", CardType.ROOM);
+        Card testWeapon = new Card("testWeapon", CardType.WEAPON);
+        Card wrongWeapon = new Card("wrongWeapon", CardType.WEAPON);
+        Card wrongPerson = new Card("wrongPerson", CardType.PERSON);
+        Card wrongRoom = new Card("wrongRoom", CardType.ROOM);
+        Card disproveRoom = new Card("disproveRoom", CardType.ROOM);
+
+        testPlayer.updateHand(testPerson);
+        testPlayer.updateHand(testRoom);
+        testPlayer.updateHand(testWeapon);
+        disprovePlayer.updateHand(disproveRoom);
+        board.addPlayer(testPlayer);
+        board.addPlayer(disprovePlayer);
+
+        // Test when player cannot disprove any cards
+        assertNull(testPlayer.disproveSuggestion(wrongPerson, wrongRoom, wrongWeapon));
+        // Player can disprove testPerson
+        assertNotNull(testPlayer.disproveSuggestion(testPerson, wrongRoom, wrongWeapon));
+        // Test where suggesting player has the cards
+        assertNull(board.handleSuggestion(testPlayer, wrongPerson, wrongRoom, wrongWeapon));
+        // disprovePlayer can disprove room card
+        assertNotNull(board.handleSuggestion(testPlayer, wrongPerson, disproveRoom, wrongWeapon));
+
+        Player testPlayer2 = new HumanPlayer("test2", "red", 7, 7);
+        testPlayer2.updateHand(testPerson);
+        Card c = board.handleSuggestion(testPlayer, testPerson, disproveRoom, wrongWeapon);
+        // Test that disproveRoom was returned not testPerson since testPlayer2 is last in list
+        assertEquals(c.getCardName(), "disproveRoom");
     }
 }
