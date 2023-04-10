f0a775d7ea0b72e6c0a0153ada1952288347406f
spencer <spencernhamilton@gmail.com>
2023-04-10 17:07:22 -0600

    broken still

--- src/clueGame/CardPanel.java
+++ src/clueGame/CardPanel.java
@@ -5,16 +5,20 @@ import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class CardPanel extends JPanel {
     private static final Board board = Board.getInstance();
+    private final JPanel peoplePanel;
+    private final JPanel weaponsPanel;
+    private final JPanel roomsPanel;
 
     public CardPanel() {
         JPanel cardPanel = new JPanel();
         setLayout(new GridLayout(3, 0));
-        JPanel peoplePanel = new JPanel();
-        JPanel roomsPanel = new JPanel();
-        JPanel weaponsPanel = new JPanel();
+        peoplePanel = new JPanel();
+        roomsPanel = new JPanel();
+        weaponsPanel = new JPanel();
         peoplePanel.setLayout(new GridLayout(0, 1));
         roomsPanel.setLayout(new GridLayout(0, 1));
         weaponsPanel.setLayout(new GridLayout(0, 1));
@@ -24,7 +28,6 @@ public class CardPanel extends JPanel {
         JLabel handLabel = new JLabel("In hand");
         JLabel seenLabel = new JLabel("Seen");
         peoplePanel.add(handLabel);
-        peoplePanel.add(seenLabel);
         JPanel handPanel = new JPanel();
 
         JPanel seenPanel = new JPanel();
@@ -32,20 +35,21 @@ public class CardPanel extends JPanel {
         seenPanel.setLayout(new GridLayout(0, 1));
 
         peoplePanel.add(handPanel);
+        peoplePanel.add(seenLabel);
         peoplePanel.add(seenPanel);
+        updateWeaponPanel();
         handPanel.add(new JLabel("test"));
         seenPanel.add(new JLabel("test"));
         add(peoplePanel);
         add(roomsPanel);
         add(weaponsPanel);
-//        cardPanel.add(peoplePanel);
-//        cardPanel.add(roomsPanel);
-//        cardPanel.add(weaponsPanel);
 
 
     }
 
     public static void main(String[] args) {
+        board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+        board.initialize();
         CardPanel cardPanel = new CardPanel();
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,8 +59,14 @@ public class CardPanel extends JPanel {
         cardPanel.setVisible(true);
     }
 
-    public void updateCardPanel(JPanel panel, CardType type) {
-        panel.removeAll();
-
+    public void updateWeaponPanel() {
+        ArrayList<Card> cards = board.getPlayers().get(0).getHand();
+        for (Card c : cards) {
+            System.out.println(c.getCardName());
+            weaponsPanel.removeAll();
+            if (c.getType() == CardType.WEAPON) {
+                weaponsPanel.add(new JLabel(c.getCardName()));
+            }
+        }
     }
 }
