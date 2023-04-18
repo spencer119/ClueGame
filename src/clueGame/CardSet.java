f2a6abbedc21afb68752f2b69a8274c836158546
Spencer Hamilton <shamilton@mines.edu>
2023-04-17 23:35:36 -0600

    Added comments for my custom CardSet class

--- src/clueGame/CardSet.java
+++ src/clueGame/CardSet.java
@@ -1,11 +1,17 @@
 package clueGame;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Set of Cards with custom functions specifically for Cards
+ */
 public class CardSet extends HashSet<Card> {
+    /**
+     * @param name Name of the card to search for
+     * @return True if the card is in the set, false otherwise
+     */
     public boolean containsName(String name) {
         for (Card c : this) {
             if (c.getCardName().equals(name)) {
@@ -15,6 +21,10 @@ public class CardSet extends HashSet<Card> {
         return false;
     }
 
+    /**
+     * @param type Type of card to search for
+     * @return Set of cards of the given type
+     */
     public Set<Card> getCardsByType(CardType type) {
         Set<Card> cards = new HashSet<>();
         for (Card c : this) {
@@ -25,6 +35,10 @@ public class CardSet extends HashSet<Card> {
         return cards;
     }
 
+    /**
+     * @param deck The deck to compare against
+     * @return Set of cards that are in the deck but not in the set
+     */
     public Set<Card> getUnseenCards(ArrayList<Card> deck) {
         Set<Card> unseen = new HashSet<>();
         for (Card c : deck) {
